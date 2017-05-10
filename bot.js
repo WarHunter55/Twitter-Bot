b8a29ac952adf65200d5bb19c54e49c009590b1a
WarHunter55 <18DiSalvo.Raymond@providencehigh.org>
2017-05-10 13:29:40 -0700

    bot change
    var picUpload

--- bot.js
+++ bot.js
@@ -126,7 +126,7 @@ var fs = require('fs');
 //processing();
 setInterval(processing, 10000);
 function processing(){
-	picUpdate();
+	var = picUpdate();
     console.log("uploaded image");
     var filename = 'pictures/image' + p + '.jpg';
     
